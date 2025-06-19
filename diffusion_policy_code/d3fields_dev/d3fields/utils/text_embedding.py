9af3b13fbd06e20291a093a3f3bf511c02d91c97
lyttttt3333 <135998170+lyttttt3333@users.noreply.github.com>
2025-06-20 01:06:27 +0800

    Update text_embedding.py

--- diffusion_policy_code/d3fields_dev/d3fields/utils/text_embedding.py
+++ diffusion_policy_code/d3fields_dev/d3fields/utils/text_embedding.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 
 def from_text_to_embedding(text):
-    client = OpenAI(api_key="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")
+    client = OpenAI(api_key=None)
     response = client.embeddings.create(input=text, model="text-embedding-ada-002")
     embedding = response.data[0].embedding
     embedding = np.array(embedding)
